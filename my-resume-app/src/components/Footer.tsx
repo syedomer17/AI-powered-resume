bff5a9918703a252cba316287f2b4d4de3fbf051
syedomer17 <syedomerali2006@gmail.com>
2025-07-06 22:22:11 +0530

    Updated by Omer on 2025-07-06 at 10:22PM

--- my-resume-app/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full mt-12 border-t p-4 text-center text-gray-500 text-sm">
-      © {new Date().getFullYear()} Your Company. All rights reserved.
-    </footer>
-  );
-}
